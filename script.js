74f8ea147e7eb646cda052c170bf358c71e63d37
emon <emondas457@gmail.com>
2022-10-15 18:33:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById("add-todo").addEventListener("click", () => {
+    let getTodo = JSON.parse(localStorage.getItem("todo-list"));
+    const inputtext = document.getElementById("input-todo");
+    let inputValue = inputtext.value;
+
+
+    if (!getTodo) {
+        const list = [
+            {
+                todo: inputValue,
+            }
+        ];
+        localStorage.setItem("todo-list", JSON.stringify(list));
+    }
+    else {
+        const list = [
+            ...getTodo,
+            {
+                todo: inputValue,
+            }
+        ];
+        localStorage.setItem("todo-list", JSON.stringify(list));
+    }
+    inputtext.value = "";
+    getTodoList()
+
+});
+
+const getTodoList = () => {
+    const TodoList = JSON.parse(localStorage.getItem("todo-list"));
+    let ulvalue = document.getElementById("todo-list");
+
+    TodoList.forEach(elements => {
+        let li = document.createElement("li");
+        li.classList = ("bg-red-600 p-2 rounded mt-2 w-full");
+        li.innerText = elements.todo;
+        ulvalue.appendChild(li);
+    });
+
+
+
+}
+getTodoList();
