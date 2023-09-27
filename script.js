5789d578f683acaf9b05b391df88c2efc9d20267
emon kumar das <89456741+EmonKumarDas@users.noreply.github.com>
2023-09-26 22:51:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.getElementById("add-todo").addEventListener("click", () => {
     const inputtext = document.getElementById("input-todo");
     let inputValue = inputtext.value;
 
-
     if (!getTodo) {
         const list = [
             {
@@ -11,8 +10,7 @@ document.getElementById("add-todo").addEventListener("click", () => {
             }
         ];
         localStorage.setItem("todo-list", JSON.stringify(list));
-    }
-    else {
+    } else {
         const list = [
             ...getTodo,
             {
@@ -22,25 +20,57 @@ document.getElementById("add-todo").addEventListener("click", () => {
         localStorage.setItem("todo-list", JSON.stringify(list));
     }
     inputtext.value = "";
-    getTodoList()
-
+    getTodoList();
 });
+
+const deleteTodo = (index) => {
+    let getTodo = JSON.parse(localStorage.getItem("todo-list"));
+    getTodo.splice(index, 1);
+    localStorage.setItem("todo-list", JSON.stringify(getTodo));
+    getTodoList();
+};
+
+const editTodo = (index) => {
+    let getTodo = JSON.parse(localStorage.getItem("todo-list"));
+    const updatedTodo = prompt("Edit your task:", getTodo[index].todo);
+    if (updatedTodo !== null) {
+        getTodo[index].todo = updatedTodo;
+        localStorage.setItem("todo-list", JSON.stringify(getTodo));
+        getTodoList();
+    }
+};
 
 const getTodoList = () => {
     const TodoList = JSON.parse(localStorage.getItem("todo-list"));
     let ulvalue = document.getElementById("todo-list");
-    ulvalue.innerText = "";
-    TodoList.forEach(elements => {
-        let li = document.createElement("li");
-        li.classList = ("bg-red-600 p-2 rounded mt-2 w-full");
-        li.innerText = elements.todo;
-        ulvalue.appendChild(li);
-    });
-}
-getTodoList();
+    ulvalue.innerHTML = "";
 
-let ClearData = () => {
+    if (TodoList) {
+        TodoList.forEach((element, index) => {
+            let li = document.createElement("li");
+            li.classList = "bg-red-600 p-2 rounded mt-2 w-full";
+
+            let deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.classList = "text-white bg-red-600 w-20 h-8 rounded mr-2";
+            deleteButton.addEventListener("click", () => deleteTodo(index));
+
+            let editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+            editButton.classList = "text-white bg-blue-600 w-20 h-8 rounded";
+            editButton.addEventListener("click", () => editTodo(index));
+
+            li.appendChild(document.createTextNode(element.todo));
+            li.appendChild(deleteButton);
+            li.appendChild(editButton);
+            ulvalue.appendChild(li);
+        });
+    }
+};
+
+const ClearData = () => {
     localStorage.removeItem("todo-list");
     getTodoList();
-}
+};
 
+getTodoList();
